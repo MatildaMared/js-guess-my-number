a8963ccfa74fc0fa648f362de14922b9bbaec691
MatildaMared <matildamared@live.se>
2021-02-22 09:04:01 +0100

    add highscore functionality

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 'use strict';
 
+// Variables
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highscore = 0;
 
+// Game functionality
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value); 
     console.log(typeof guess, guess);
 
     if (!guess) {
-        //When there is no input
+        // When there is no input
         document.querySelector(".message").textContent = "😟 No number!";
 
-        //Player wins
+        // Player wins
     } else if (guess === secretNumber) {
         document.querySelector(".message").textContent = "🥳 Correct number!";
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector(".number").style.width = "30rem";
 
-        //Guess is too high
+        // Update highscore
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+        }
+
+        // Guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '📈 Too high!';
@@ -30,7 +38,7 @@ document.querySelector(".check").addEventListener("click", function () {
             document.querySelector(".score").textContent = 0;
         }
 
-        //Guess is too low
+        // Guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '📉 Too low!';
@@ -46,7 +54,7 @@ document.querySelector(".check").addEventListener("click", function () {
 
 // Restore the game
 document.querySelector(".again").addEventListener("click", function () {
-    
+
     // Resetting background color
     document.querySelector("body").style.backgroundColor = "mediumpurple";
 
@@ -57,7 +65,7 @@ document.querySelector(".again").addEventListener("click", function () {
 
     // Resetting score
     score = 20;
-    document.querySelector(".score").textContent = 20;
+    document.querySelector(".score").textContent = score;
 
     // Resetting message
     document.querySelector(".message").textContent = "Start guessing...";
